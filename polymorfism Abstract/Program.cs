9caba2942a639f9c350e957310ebe29ab0e44544
mikaparn <mikaparn@LSE-B3984>
2019-11-14 11:32:56 +0100

    Add project files.

---
+++ polymorfism Abstract/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace polymorfism_Abstract
+{
+    class Player
+    {
+        public void Attack()
+        {
+            Console.WriteLine("A Player attacks!");
+        }
+    }
+    class Mage : Player
+    {
+        public void Attack()
+        {
+            Console.WriteLine("A Mage attacks with a fireball!");
+        }
+    }
+    class Warrior : Player
+    {
+        public void Attack()
+        {
+            Console.WriteLine("A Warrior attacks with a sword!");
+        }
+    }
+    class program
+    {
+        static void Main(string[] args)
+        {
+            List<Player> players = new List<Player>();
+
+            players.Add(new Player());
+            players.Add(new Mage());
+            players.Add(new Warrior());
+
+            foreach (Player p in players)
+            {
+                p.Attack();
+            }
+        }
+    }
+}
